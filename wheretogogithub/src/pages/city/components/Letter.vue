d995788c5bbb8e846374bb00d94437b41e70def4
huangyuzai <1792132332@qq.com>
2018-09-23 14:30:22 +0800

    optimization

--- wheretogogithub/src/pages/city/components/Letter.vue
+++ wheretogogithub/src/pages/city/components/Letter.vue
@@ -24,8 +24,13 @@ export default {
     },
     data () {
         return {
-            touchstatus: false
+            touchstatus: false,
+            startY: 0,
+            timer: null
         }
+    },
+    updated () {
+        this.startY = this.$refs['A'][0].offsetTop
     },
     methods: {
         letterClick (e) {
@@ -36,12 +41,16 @@ export default {
         },
         touchmove (e) {
             if(this.touchstatus) {
-                const startY = this.$refs['A'][0].offsetTop
-                const touchY = e.touches[0].clientY
-                const index = Math.floor((touchY - startY) / 15)
-                if(index >= 0 && index < this.letters.length) {
-                    this.$emit('change',this.letters[index])
+                if(this.timer) {
+                    clearTimeout(this.timer)
                 }
+                this.timer = setTimeout(() => {
+                    const touchY = e.touches[0].clientY
+                    const index = Math.floor((touchY - this.startY) / 15)
+                    if(index >= 0 && index < this.letters.length) {
+                        this.$emit('change',this.letters[index])
+                    }
+                },16)
             }
         },
         touchend () {
