97f956be9a455d7b852c97bfeca78284832aa0a8
huangyuzai <1792132332@qq.com>
2018-09-23 16:13:06 +0800

    city-search-optimization

--- wheretogogithub/src/pages/city/components/Search.vue
+++ wheretogogithub/src/pages/city/components/Search.vue
@@ -1,12 +1,74 @@
 <template>
-    <div class="search">
-        <input type="text" class="search-input" placeholder="输入城市名或拼音" />
+    <div>
+        <div class="search">
+            <input
+                type="text"
+                class="search-input"
+                placeholder="输入城市名或拼音"
+                v-model='keyWord'
+            />
+        </div>
+        <div class="search-content"
+             v-show='hasNoData'
+             ref='content'
+        >
+            <ul class="content-box">
+                <li class="content-title"
+                    v-for='item of list'
+                    :key='item.id'
+                >
+                {{item.name}}</li>
+                <li class="content-title noData"
+                    v-show='noData'
+                >没有找到匹配的数据~</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-    name: 'CitySearch'
+    name: 'CitySearch',
+    props: {
+        cities: Object
+    },
+    data () {
+        return {
+            keyWord: '',
+            list: [],
+            timer: null
+        }
+    },
+    computed: {
+        hasNoData () {
+            return this.keyWord.length
+        },
+        noData () {
+            return !this.list.length
+        }
+    },
+    watch: {
+        keyWord () {
+            if(this.timer) {
+                clearTimeout(this.timer)
+            }
+            this.timer = setTimeout( () => {
+                const result = []
+                for(let i in this.cities) {
+                    this.cities[i].forEach((value) => {
+                        if(value.spell.indexOf(this.keyWord) > -1 || value.name.indexOf(this.keyWord) > -1) {
+                            result.push(value)
+                        }
+                    })
+                }
+                this.list = result
+            },100)
+        }
+    },
+    mounted () {
+        this.scroll = new Bscroll(this.$refs.content)
+    }
 }
 </script>
 
@@ -22,4 +84,21 @@ export default {
             text-align: center
             border-radius: .06rem
             color: #666
+    .search-content
+        box-sizing: border-box
+        overflow: hidden
+        background: #eee
+        position: absolute
+        top: 1.6rem
+        left: 0
+        right: 0
+        bottom: 0
+        z-index: 1
+        .content-box
+            .content-title
+                padding: .2rem
+                background: white
+                border-bottom: solid 1px $borderColor
+            .noData
+                text-align: center
 </style>
